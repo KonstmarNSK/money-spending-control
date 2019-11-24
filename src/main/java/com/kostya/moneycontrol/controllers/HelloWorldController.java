f5ca5220af6678ee464bef1161a447298c1d39b3
KonstmarNSK <konstmar@yandex.ru>
2019-11-24 13:26:19 +0600

    issue#1: added ResponseBody annotation

--- src/main/java/com/kostya/moneycontrol/controllers/HelloWorldController.java
+++ src/main/java/com/kostya/moneycontrol/controllers/HelloWorldController.java
@@ -2,9 +2,11 @@ package com.kostya.moneycontrol.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+    @ResponseBody
     @GetMapping("/sayHello")
     public String sayHello(){
         return "Hello, World!";
